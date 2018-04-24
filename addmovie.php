13ad6b0b890dd746f7534b225813215f887467e9
McKensy <33714767+McKensy@users.noreply.github.com>
2018-04-24 21:53:34 +0200

    addmovie.php css done.

--- addmovie.php
+++ addmovie.php
@@ -17,31 +17,49 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8" />
-        <link rel="stylesheet" href="style.css">
-        <title>Movie2k</title>
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Font awesome link-->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+      <title>Movie2k</title>
     </head>
-    <body>
-        <div class="main">
-            <h1>Add a movie</h1>
-            <div class="centerbox">
+    <body style="background-color: #555555">
+      <nav>
+          <div class="nav-wrapper teal lighten-2">
+              <a href="#">  Logged in as <?php echo $name ?></a>
+              <a href=".\" class="brand-logo center">Movies</a>
+              <ul id="nav-mobile" class="right">
+                  <li><a href=".\logout.php">Logout</a></li>
+                  <li><a href="#">Add Movie</a></li>
+              </ul>
+          </div>
+      </nav>
+        <div class="container">
+          <div class="card-panel" style="margin: 1% 1% 1% 1%">
+            <h3 class="center">Add a movie</h3>
+            <div class="centerbox" style="margin: 1% 1% 1% 1%">
                 <form action="./addmovie.php" method="post">
-                    <p>Name:    <input type="text" name="moviename" placeholder="Movie name" maxlength="64" required/> </p>
-                    <p>Subtitle:    <input type="text" name="subtitle" placeholder="Subtitle" maxlength="64" required/> </p>
-                    <p>Description:    <input type="text" name="description" placeholder="Description" maxlength="512" required/> </p>
-                    <p>Video-embed link:    <input type="text" name="trailer" placeholder="Trailer" maxlength="128" required/> </p>
-                    <div class="select-style">
-                        <select name="dropdown">
-                            <option value="0" selected disabled hidden>Category</option>
+                    <p>Name:    <input type="text" name="moviename" class="validate" placeholder="The name of the Movie" maxlength="64" required/> </p>
+                    <p>Subtitle:    <input type="text" name="subtitle" placeholder="Example: 7.7/10 Stars | 1h 44min | Horror, Mystery, Thriller | 24 February 2017 (USA)" maxlength="64" required/> </p>
+                    <p>Description:    <input type="text" name="description" placeholder="A little description of the movie." maxlength="512" required/> </p>
+                    <p>Video-embed link:    <input type="text" name="trailer" placeholder="Example: https://www.youtube.com/embed/watch?v=-40fLtf9Hio" maxlength="128" required/> </p>
+                    <div class="input-field hoverable">
+                        <select name="dropdown" class="browser-default" required>
+                            <option value="0" selected disabled>Category</option>
                             <option value="1">Comedy</option>
                             <option value="2">Horror</option>
                             <option value="3">Sci-Fi</option>
                             <option value="4">Animation</option>
                         </select>
                     </div>
-                    <button class="select-button" type="Submit" name="Submit">Submit</button>
+                    <button class="btn waves-effect waves-light" type="Submit" name="Submit">Submit<i class="material-icons right">send</i></button>
                 </form>
             </div>
+          </div>
         </div>
         <?php
         function alert($msg) {
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
         <title>Movie2k</title>
     </head>
-    <body style="background-color: grey;">
+    <body style="background-color: #555555;">
         <nav>
             <div class="nav-wrapper teal lighten-2">
                 <a href=".\">  Logged in as <?php echo $name ?></a>
